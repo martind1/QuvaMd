2111941df62c6717d3f2aca2aff9dd27251ecc96
martind1 <mdbsoft@gmail.com>
2023-04-08 23:32:27 +0200

    EasyModbus

---
+++ Quva.Services/GlobalSuppressions.cs
@@ -0,0 +1,15 @@
+﻿// This file is used by Code Analysis to maintain SuppressMessage
+// attributes that are applied to this project.
+// Project-level suppressions either have no target or are given
+// a specific target and scoped to a namespace, type, member, etc.
+
+using System.Diagnostics.CodeAnalysis;
+
+[assembly: SuppressMessage("Style", "IDE0090:Use 'new(...)'", Justification = "foreign code", Scope = "namespaceanddescendants", Target = "~N:Quva.Services.Devices.EasyModbus")]
+[assembly: SuppressMessage("Style", "IDE1005:Delegate invocation can be simplified.", Justification = "foreign code", Scope = "namespaceanddescendants", Target = "~N:Quva.Services.Devices.EasyModbus")]
+[assembly: SuppressMessage("Style", "IDE0056:Use index operator", Justification = "foreign code", Scope = "namespaceanddescendants", Target = "~N:Quva.Services.Devices.EasyModbus")]
+[assembly: SuppressMessage("Style", "IDE0044:Add readonly modifier", Justification = "foreign code", Scope = "namespaceanddescendants", Target = "~N:Quva.Services.Devices.EasyModbus")]
+[assembly: SuppressMessage("Style", "IDE0031:Use null propagation", Justification = "foreign code", Scope = "namespaceanddescendants", Target = "~N:Quva.Services.Devices.EasyModbus")]
+[assembly: SuppressMessage("Style", "IDE0017:Simplify object initialization", Justification = "foreign code", Scope = "namespaceanddescendants", Target = "~N:Quva.Services.Devices.EasyModbus")]
+[assembly: SuppressMessage("Style", "IDE0059:Unnecessary assignment of a value", Justification = "foreign code", Scope = "namespaceanddescendants", Target = "~N:Quva.Services.Devices.EasyModbus")]
+
